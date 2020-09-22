ff74a014f93b9bb61083c20d851f901b200fdecd
Snailey <jaran.martinsen@online.no>
2020-09-22 20:48:07 +0200

    added data-clases

--- app/src/main/java/no/hiof/snailey/familyplaner/model/shopping.java
+++ app/src/main/java/no/hiof/snailey/familyplaner/model/shopping.java
@@ -1,4 +1,48 @@
 package no.hiof.snailey.familyplaner.model;
 
 public class shopping {
+
+    private String title;
+    private int number;
+    private boolean active;
+    private String family;
+
+    public shopping(String title, int number, boolean active, String family) {
+        this.title = title;
+        this.number = number;
+        this.active = active;
+        this.family = family;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
 }
--- app/src/main/java/no/hiof/snailey/familyplaner/model/todo.java
+++ app/src/main/java/no/hiof/snailey/familyplaner/model/todo.java
@@ -1,4 +1,48 @@
 package no.hiof.snailey.familyplaner.model;
 
 public class todo {
+
+    private String title;
+    private String description;
+    private boolean active;
+    private String family;
+
+    public todo(String title, String description, boolean active, String family) {
+        this.title = title;
+        this.description = description;
+        this.active = active;
+        this.family = family;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
 }
